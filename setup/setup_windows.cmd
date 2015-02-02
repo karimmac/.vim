35df6250b14be2f05e737492d1d526547ec76965
Karim MacDonald <karim.macdonald@gmail.com>
2015-02-02 13:41:58 +0000

    Use %HOME% on Windows rather than %USERPROFILE%

--- setup/setup_windows.cmd
+++ setup/setup_windows.cmd
@@ -1,5 +1,5 @@
 rem Linking might work on Vista+; it doesn't work well on XP.
 rem mklink requires administrator privileges.
 
-mklink /d "%USERPROFILE%\vimfiles" "%~dp0.."
-mklink "%USERPROFILE%\.vimrc" "%~dp0..\.vimrc"
+mklink /d "%HOME%\vimfiles" "%~dp0.."
+mklink "%HOME%\.vimrc" "%~dp0..\.vimrc"
